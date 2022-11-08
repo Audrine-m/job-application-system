2097cef77780f013f732b3483a8fcbfc7fed7cea
levin-mutai <levinmutai2@gmail.com>
2022-11-08 11:44:39 +0300

    my new development branch

--- src/pages/TrackApllication.vue
+++ src/pages/TrackApllication.vue
@@ -1,7 +1,3 @@
 <template>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis eos placeat ex
-    consectetur facilis sed sapiente aut beatae voluptatibus, est assumenda modi nisi
-    dolorum aliquid recusandae dolore harum cum incidunt?
-  </div>
+  <div></div>
 </template>
